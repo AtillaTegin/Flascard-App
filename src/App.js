0a612bed35f4fef817d12af23b607ae2b0fb6d8e
AtillaTegin <100026159+AtillaTegin@users.noreply.github.com>
2024-01-03 01:12:36 +0400

    Created App.js part and some other neccessary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import FlashCardsPage from './components/FlashCardsPage'; // You need to create this component
+import ContactMe from './components/ContactMe';
+import Navigation from './components/Navigation';
 
 function App() {
+  const [flashCards, setFlashCards] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const API_URL = 'http://localhost:3000/my-app/db.json'; // Assuming your json-server is running on this port
+
+  useEffect(() => {
+    const fetchFlashCards = async () => {
+      try {
+        const res = await fetch(API_URL);
+        if (!res.ok) throw Error('Data has not been received');
+        const flashCardsData = await res.json();
+        setFlashCards(flashCardsData);
+        setFetchError(null);
+      } catch (e) {
+        setFetchError(e.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    // Fetch data when the component mounts
+    fetchFlashCards();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/flash-cards">
+          <FlashCardsPage
+            flashCards={flashCards}
+            fetchError={fetchError}
+            isLoading={isLoading}
+          />
+        </Route>
+        <Route path="/contact-me">
+          <ContactMe />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
